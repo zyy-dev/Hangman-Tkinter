974e21d737da48a90b3430adae3831803eeed50e
Richard Torculas <144353934+richardtorculas@users.noreply.github.com>
2024-04-28 12:39:24 +0800

    Update words.py

--- Frames/game/words.py
+++ Frames/game/words.py
@@ -4,22 +4,30 @@ class Words:
     def __init__(self):
         self.words = {
            1: {
-        "Animals 1": ['cat', 'dog', 'bat', 'fox', 'cow', 'owl', 'pig', 'goat', 'rat', 'bird', 'lion'],
-        "Body Parts": ['arm', 'eye', 'head', 'neck', 'jaw', 'ear', 'hand', 'foot', 'hip'],
-        "Food": ['pie', 'jam', 'tea', 'egg', 'beef', 'rice', 'jam', 'pie', 'pork', 'cake']},
+        "Colors 1": ['red', 'blue', 'green', 'black', 'white', 'pink', 'cyan'],
+        "Numbers": ['two', 'one', 'ten', 'nine', 'five', 'six'],
+        "Animals 1": ['cat', 'dog', 'bat', 'fox', 'cow', 'owl', 'pig', 'goat', 'rat', 'bird', 'lion']},       
            2: {
+        "Animals 2": ['wolf', 'deer', 'lion', 'seal', 'bear', 'worm', 'frog', 'hawk', 'crow', 'duck', 'crab'],
+        "Body Parts": ['arm', 'eye', 'head', 'neck', 'jaw', 'ear', 'hand', 'foot', 'hip'],
+        "Food": ['pie', 'jam', 'tea', 'egg', 'beef', 'rice', 'jam', 'pork', 'cake']},
+            3: {
+        "Relatives": ['sister', 'brother', 'father', 'mother', 'aunt', 'uncle'],
+        "Colors 2": ['brown', 'magenta', 'indigo', 'yellow', 'maroon', 'purple'],
+        "Fruit": ['apple', 'lemon', 'banana', 'mango', 'orange', 'cherry', 'peach', 'guava', 'grapes', 'pear']},
+           4: {
         "Country": ['spain', 'france', 'mexico', 'india', 'norway', 'china', 'japan'],
         "Sports": ['soccer', 'boxing', 'hockey', 'skating', 'archery', 'bowling'],
         "Musical Instruments": ['violin', 'guitar', 'piano', 'flute', 'trumpet', 'ukulele', 'triangle']},
-           3: {
-        "Animals 2": ['sloth', 'skunk', 'otter', 'goose', 'raven', 'moose', 'llama', 'koala', 'hyena', 'bison'],
+           5: {
+        "Animals 3": ['sloth', 'skunk', 'otter', 'goose', 'raven', 'moose', 'llama', 'koala', 'hyena', 'bison'],
         "Professions": ['firefighter', 'politician', 'musician', 'journalist', 'surgeon', 'psychiatrist', 'astronomer'],
         "Superheroes": ['flash', 'darna', 'groot', 'rocket', 'batman', 'hawkeye', 'shazam']},
-           4: {
+           6: {
         "Fast Food Chains": ['jollibee', 'chowking', 'greenwich', 'kfc', 'subway', 'mcdonald', 'starbucks', 'chipotle', 'popeyes'],
         "Greek Gods 1": ['poseidon', 'hera', 'athena', 'zeus', 'apollo', 'ares', 'aphrodite', 'hades'],
         "Car Brands": ['porsche', 'chevrolet', 'cadillac', 'dodge', 'bugatti', 'volkswagen', 'mclaren', 'maserati']},
-           5: {
+           7: {
         "Greek Gods 2": ['demeter', 'dionysus', 'persephone', 'hestia', 'hephaestus', 'artemis', 'cronus', 'prometheus'],
         "Alcohol Brands": ['bacardi', 'hennessy', 'smirnoff', 'baileys', 'emperador', 'ginebra', 'fundador'],
         "Mythology": ['phoenix', 'cerberus', 'werewolf', 'sphinx', 'centaur', 'minotaur', 'chimera', 'unicorn']},
